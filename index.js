4db614803b7ecb6383a049ba208dfba61bdfff58
coby walsh <cobywalsh7@yahoo.com>
2023-06-15 17:13:45 -0600

    added code for dynamic questions in my terminal

---
+++ index.js
@@ -0,0 +1,57 @@
+var inquirer = require('inquirer');
+const fs = require('fs');
+const fileName = 'README.md';
+
+.prompt([
+    {
+        type: "input",
+        name: "titleQuestion",
+        message: "What is the title of my project"
+    },
+    {
+        type: "input",
+        name: "descriptionQuestion",
+        message: "What is the description of my project"
+    },
+    {
+        type: "input",
+        name: "instalationQuestion",
+        message: "What is the installation instructions of my project"
+    },
+    {
+        type: "input",
+        name: "usageInformationQuestion",
+        message: "What is the usage information for my project"
+    }, 
+    {
+        type: "input",
+        name: "contributionGuidelinesQuestion",
+        message: "What is the contribution guidelines for my project"
+    },
+    {
+        type: "input",
+        name: "testInstructionsQuestion",
+        message: "What are the test instuctions for my project"
+    },
+])
+.then((error) => {
+    console.log(answers);
+    fs.appendFile("README.md", answers.nameQuestion, (error) => error ? console.log(error) : console.log("Success!"));
+})
+.catch((error) => {
+    if (error.isTtyError) {
+    console.log("There is an error");        
+    }
+})
+function writeToReadMe(fileName, answers) {
+    fs.writeFile(fileName, answers, err => {
+        err ? console.error(err) : console.log('readme file created!')
+    });
+    var title = answers.titleQuestion;
+    var description = answers.descriptionQuestion;
+    var installation = answers.installationQuestion;
+    var usage = answers.usageInformationQuestion;
+    var contribution = answers.contributionGuidelinesQuestion;
+    var testInstructions = answers.testInstructionsQuestion;
+    // var readmeToWrite = README.md
+}
